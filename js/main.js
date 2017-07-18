dbb85cea274eb951aacc387b5bae92ff9b2ec6af
cyrilforrez <cyilforrez@gmail.com>
2017-07-18 10:38:44 +0200

    create random and change img

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-var images = ['cra.png', 'Ecaflip.png', 'Eniripsa.png', 'Feca.png', 'sacri.png', 'Sram.png', 'Xelor.png', 'cra.png', 'Ecaflip.png', 'Eniripsa.png', 'Feca.png', 'sacri.png', 'Sram.png', 'Xelor.png'];
-
+var images = ['img/pc/cra.png', 'img/pc/Ecaflip.png', 'img/pc/Eniripsa.png', 'img/pc/Feca.png', 'img/pc/sacri.png', 'img/pc/Sram.png', 'img/pc/Xelor.png', 'img/pc/cra.png', 'img/pc/Ecaflip.png', 'img/pc/Eniripsa.png', 'img/pc/Feca.png', 'img/pc/sacri.png', 'img/pc/Sram.png', 'img/pc/Xelor.png'];
+var image = document.getElementsByTagName('img');
+console.log(images);
 
 function shuffleArray(images) {
     for (var i = images.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = images[i];
         images[i] = images[j];
-        images[j] = images;
+      
+        images[j] = temp;
     }
-    return images;
+
 }
 
-var x = document.getElementByClassName("flex-item");
-if (x.addEventListener) {
-    x.addEventListener("click", retourner);
-    } else if (x.attachEvent) {
-    x.attachEvent("onclick");
-}
-
-function retourner() {
-    
-}
+shuffleArray(images);
+console.log(images);
+for (let i = 0; i < images.length; i++) {
+	
+	image[i].addEventListener("click", function () {
+		this.src=images[i];}
+	
+	) };
