0a303082cc3e11d5d5982f5fd36e192354720b37
cyrilforrez <cyilforrez@gmail.com>
2017-07-19 14:42:55 +0200

    Adding the counter to confirm, reloading the page and the good bye page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var image = document.getElementsByTagName('img');
 var source = "";
 var imgBack = image[0].src;
 var clickLock = 0;
-var compteur =0;
+var compteur = 0;
+var confirmcompt = 0;
 
 
 //------------------------------------------Random Function---------------------------------------
@@ -41,8 +42,9 @@ function comparer(index) {
 
         } else {
             image[index].src = images[index];
-            compteur++;
+
             if (source.src != image[index].src) { //----------Compare the 2 cards back
+                compteur++;    //--------------------Add 1 point for each pair not found
                 clickLock = 1; //-----------Blocks the click if 2 cards are returned
                 setTimeout(function() {
                     clickLock = 0;
@@ -52,11 +54,20 @@ function comparer(index) {
                 }, 1500);
             } else {
                 source = "";
+                confirmcompt++;    //-----------------------Add 1 point for each pair find
             }
 
+
         }
-        document.getElementById('compteur').innerHTML = "Nombre d'essaie" +":" +" " +compteur;
     }
-
-
+    document.getElementById('compteur').innerHTML = "Nombre d'essaie" + ":" + " " + compteur; //----Displays the counter
+    if ((confirmcompt) == 7) {
+        alert("Vous avez gagné!");
+        if (confirm = confirm("Voulez vous rejouer?")) { //--------------Output option
+            window.location.reload(); //--------------Reload page
+        } else {
+            document.getElementById("end").style.display = 'block'; //---------Poster page goodbye
+            document.getElementById("end").style.zIndex = '1';
+        }
+    }
 }
