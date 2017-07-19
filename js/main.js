749fa4cd71d0912cf10b38d224540830b38289ea
cyrilforrez <cyilforrez@gmail.com>
2017-07-19 09:34:15 +0200

    create counter

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var image = document.getElementsByTagName('img');
 var source = "";
 var imgBack = image[0].src;
 var clickLock = 0;
+var compteur =0;
 
 
 //------------------------------------------Random Function---------------------------------------
@@ -21,7 +22,7 @@ function shuffleArray(images) {
 shuffleArray(images);
 for (let i = 0; i < images.length; i++) {
 
-    image[i].addEventListener("click", function() {
+    image[i].addEventListener("click", function() { //----------Returns the card to the click
             comparer(i);
         }
 
@@ -37,14 +38,16 @@ function comparer(index) {
         if (source == "") {
             image[index].src = images[index]; //----------------------Displays an image of the index
             source = image[index];
+
         } else {
             image[index].src = images[index];
-            if (source.src != image[index].src) {  //----------Compare the 2 cards back
-                clickLock = 1;  //-----------Blocks the click if 2 cards are returned
-                setTimeout(function() { 
+            compteur++;
+            if (source.src != image[index].src) { //----------Compare the 2 cards back
+                clickLock = 1; //-----------Blocks the click if 2 cards are returned
+                setTimeout(function() {
                     clickLock = 0;
                     source.src = imgBack;
-                    image[index].src = imgBack;  //------------Returns cards if they are different
+                    image[index].src = imgBack; //------------Returns cards if they are different
                     source = "";
                 }, 1500);
             } else {
@@ -52,5 +55,8 @@ function comparer(index) {
             }
 
         }
+        document.getElementById('compteur').innerHTML = "Nombre d'essaie" +":" +" " +compteur;
     }
+
+
 }
